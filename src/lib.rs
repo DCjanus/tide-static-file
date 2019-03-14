c8e180b5fbd6c917dd33ade0116bb21684402c95
DCjanus <DCjanus@dcjanus.com>
2019-03-14 11:25:04 +0800

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! Static file server implementation, work with [Tide](https://github.com/rustasync/tide)
+//!
+//! # TODO
+//! + Whole file response
+//! + Single range
+//! + Multi Range
+//! + ETAG
+//! + Last-Modified
+//! + Content-Disposition
+//! + Better performance
+//! + Index file support
+//! + File list for directory (default off)
